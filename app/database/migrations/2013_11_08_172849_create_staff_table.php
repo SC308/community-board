e5e3818e2fe53b5ccaed4d18a47234195b3304a9
Brent Garner <bgarner@gmail.com>
2014-07-17 10:40:18 -0600

    adding database migrations from Yonge as a starting point

---
+++ app/database/migrations/2013_11_08_172849_create_staff_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('StaffBios', function($table) {
+			$table->increments('id');
+			$table->string('first');
+			$table->string('last');
+			$table->string('position');
+			$table->string('photo');
+			$table->text('bio');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('StaffBios');
+	}
+
+}
---
+++ app/database/migrations/2013_11_08_224914_create_photo_collections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoCollectionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photo_collections', function($table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('location');
+			$table->timestamp('collection_date');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photo_collections');
+	}
+
+}
