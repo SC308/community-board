fee4cdff36830251cc7db1f8371efbc8e3bac7a9
Brent Garner <bgarner@gmail.com>
2014-07-19 19:40:34 -0600

    added some user messages if they enter a store # not in the db

--- app/controllers/CashController.php
+++ app/controllers/CashController.php
@@ -18,39 +18,21 @@ class CashController extends BaseController{
 
         $storedetails = Store::getStoreDetails($sn); 
 
-        return View::make('cash/cash')
-            ->with('staff', $staff)
-            ->with('talls', $talls)
-            ->with('singles', $singles)
-            ->with('events', $events)
-            ->with('photos', $photos)
-            ->with('storedetails', $storedetails);
+		if (!$storedetails) {
+			
+			return "Oops, can't find that store. Use a valid store number. <em>Example:</em> <strong>http://domain.com/<em>5111</em>/cash</strong>";
+			
+		} else {
+			
+	        return View::make('cash/cash')
+	            ->with('staff', $staff)
+	            ->with('talls', $talls)
+	            ->with('singles', $singles)
+	            ->with('events', $events)
+	            ->with('photos', $photos)
+	            ->with('storedetails', $storedetails);			
+		}
         
     }
 
-    // public function getStore($sn){
-
-    //     $staff = StaffBio::get();
-        
-    //     $talls = CashwallAsset::getTalls();
-        
-    //     $singles = CashwallAsset::getSingles();
-        
-    //     $events = CommunityEvent::getnextthirty();
-
-    //     $photos = Photo::orderBy(DB::raw('RAND()'))
-    //             ->where("publish","=",1)
-    //             ->get();
-
-    //     $storedetails = Store::getStoreDetails($sn);        
-
-    //     return View::make('cash/cash')
-    //         ->with('staff', $staff)
-    //         ->with('talls', $talls)
-    //         ->with('singles', $singles)
-    //         ->with('events', $events)
-    //         ->with('photos', $photos)
-    //         ->with('storedetails', $storedetails);
-
-    // }
 }
