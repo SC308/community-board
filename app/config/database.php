767e140be6b02a2ae4d8c3f7461cd2f6a47b3eec
Brent Garner <bgarner@gmail.com>
2014-07-19 18:32:02 -0600

    added database config to ignore

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'community_board_dev',
 			'username'  => 'root',
 			'password'  => 'root',
