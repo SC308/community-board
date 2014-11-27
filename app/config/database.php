5542a07aceb3aa86f3bb27a4f15411e88ee9793c
Brent Garner <bgarner@gmail.com>
2014-11-27 15:31:47 -0700

    changes to gitignore

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'community_board',
+			'database'  => 'community_board_dev',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
