84dc82f62a43d5a1e24f524eb6dbd1e92fc37b9d
Megha Jyoti <jyoti5337@gmail.com>
2015-04-23 09:04:09 -0600

    updated routes and dashboard

--- app/routes.php
+++ app/routes.php
@@ -79,7 +79,57 @@ Route::post('/admin/feature/saveadd', 'AdminController@saveAddFeature');
 Route::get('/admin/jumpstart', 'AdminController@getJumpstart');
 Route::post('/admin/jumpstart/savedit', 'AdminController@saveEditJumpstart');
 
+/*Activity Kiosk Routes*/
+Route::get('/admin/kiosk/{storeno}',					'KioskController@index')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/event', 				'EventController@index')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/blog', 				array('before' => 'admin', 'uses' => 'BlogController@index'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/gear', 				array('before' => 'admin', 'uses' => 'GearController@index'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/league', 			'LeagueController@index')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/location', 			'LocationController@index')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/sport', 				array('before' => 'admin', 'uses' => 'SportController@index'))->where('storeno', '[0-9]+');
 
+Route::get('/admin/kiosk/{storeno}/event/create', 		'EventController@create')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/blog/create', 		array('before' => 'admin', 'uses' => 'BlogController@create'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/gear/create', 		array('before' => 'admin', 'uses' => 'GearController@create'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/league/create', 	 	'LeagueController@create')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/location/create', 	'LocationController@create')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/sport/create', 		array('before' => 'admin', 'uses' => 'SportController@create'))->where('storeno', '[0-9]+');
+
+Route::get('/admin/kiosk/{storeno}/event/{id}', 		'EventController@show')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/blog/{id}', 			array('before' => 'admin', 'uses' => 'BlogController@show'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/gear/{id}', 			array('before' => 'admin', 'uses' => 'GearController@show'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/league/{id}', 		'LeagueController@show')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/location/{id}', 		'LocationController@show')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/sport/{id}', 		'SportController@show')->where('storeno', '[0-9]+');
+
+Route::get('/admin/kiosk/{storeno}/event/{id}/edit', 	'EventController@edit')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/blog/{id}/edit', 	array('before' => 'admin', 'uses' => 'BlogController@edit'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/gear/{id}/edit', 	array('before' => 'admin', 'uses' => 'GearController@edit'))->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/league/{id}/edit', 	'LeagueController@edit')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/location/{id}/edit', 'LocationController@edit')->where('storeno', '[0-9]+');
+Route::get('/admin/kiosk/{storeno}/sport/{id}/edit', 	'SportController@edit')->where('storeno', '[0-9]+');
+
+Route::patch('/admin/kiosk/{storeno}/event/{id}', 		array( 'as'=>'event.update', 'uses'=> 'EventController@update'));
+Route::patch('/admin/kiosk/{storeno}/blog/{id}',  		array( 'as'=>'blog.update' , 'uses'=> 'BlogController@update'));
+Route::patch('/admin/kiosk/{storeno}/gear/{id}',  		array( 'as'=>'gear.update' , 'uses'=> 'GearController@update'));
+Route::patch('/admin/kiosk/{storeno}/league/{id}',  	array( 'as'=>'league.update' , 'uses'=> 'LeagueController@update'));
+Route::patch('/admin/kiosk/{storeno}/location/{id}',  	array( 'as'=>'location.update' , 'uses'=> 'LocationController@update'));
+Route::patch('/admin/kiosk/{storeno}/sport/{id}', 		array( 'as'=>'sport.update' , 'uses'=> 'SportController@update'));
+
+Route::post('/admin/kiosk/{storeno}/event', 			array( 'as'=>'event.store', 'uses'=> 'EventController@store'));
+Route::post('/admin/kiosk/{storeno}/blog',  			array( 'as'=>'blog.store' , 'uses'=> 'BlogController@store'));
+Route::post('/admin/kiosk/{storeno}/gear',  			array( 'as'=>'gear.store' , 'uses'=> 'GearController@store'));
+Route::post('/admin/kiosk/{storeno}/league',  			array( 'as'=>'league.store' , 'uses'=> 'LeagueController@store'));
+Route::post('/admin/kiosk/{storeno}/location',  		array( 'as'=>'location.store' , 'uses'=> 'LocationController@store'));
+Route::post('/admin/kiosk/{storeno}/sport',  			array( 'as'=>'sport.store' , 'uses'=> 'SportController@store'));
+
+
+Route::delete('/admin/kiosk/{storeno}/event/{id}', 		array( 'as'=>'event.delete', 'uses'=> 'EventController@destroy'));
+Route::delete('/admin/kiosk/{storeno}/blog/{id}',  		array( 'as'=>'blog.delete' , 'uses'=> 'BlogController@destroy'));
+Route::delete('/admin/kiosk/{storeno}/gear/{id}',  		array( 'as'=>'gear.delete' , 'uses'=> 'GearController@destroy'));
+Route::delete('/admin/kiosk/{storeno}/league/{id}',  	array( 'as'=>'league.delete' , 'uses'=> 'LeagueController@destroy'));
+Route::delete('/admin/kiosk/{storeno}/location/{id}',  	array( 'as'=>'location.delete' , 'uses'=> 'LocationController@destroy'));
+Route::delete('/admin/kiosk/{storeno}/sport/{id}',  	array( 'as'=>'sport.delete' , 'uses'=> 'SportController@destroy'));
 
 /*API ROUTES*/
 Route::get('/api', 'ApiController@getIndex');
