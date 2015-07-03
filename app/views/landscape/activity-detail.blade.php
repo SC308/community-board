f5e469170bb8a88fcbc7cf24992ec8e7faa6eb42
Megha Jyoti <jyoti5337@gmail.com>
2015-07-03 13:22:49 -0600

    font for activity views added

--- app/views/landscape/activity-detail.blade.php
+++ app/views/landscape/activity-detail.blade.php
@@ -13,6 +13,8 @@
         <link rel="stylesheet" href="/css/font-awesome.css?<?=time();?>">
         <link rel="stylesheet" href="/js/fancybox/source/jquery.fancybox.css?<?=time();?>">
         <link rel="stylesheet" href="/css/main.css?<?=time();?>">
+        <link rel="stylesheet" type="text/css" href="/css/includes/activity.css">
+        
 
 		<script src="/js/lib/modernizr.min.js"></script>
 
