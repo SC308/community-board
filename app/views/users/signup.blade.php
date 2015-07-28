d2e61961bd78fea34d9abe2b1cb14343a1440998
Brent Garner <bgarner@gmail.com>
2015-07-28 13:32:21 -0600

    added store number to user creation form

--- app/views/users/signup.blade.php
+++ app/views/users/signup.blade.php
@@ -57,7 +57,64 @@
 					<div class="panel-heading"><h3 class="panel-title"><strong>Create a New Account</strong></h3></div>
 					<div class="panel-body">
             {{-- Renders the signup form of Confide --}}
-            {{ Confide::makeSignupForm()->render(); }}
+            {{-- Confide::makeSignupForm()->render(); --}}
+
+
+            <form method="POST" action="{{{ URL::to('users') }}}" accept-charset="UTF-8">
+                <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
+                <fieldset>
+                    <div class="form-group">
+                        <label for="username">{{{ Lang::get('confide::confide.username') }}}</label>
+                        <input class="form-control" placeholder="{{{ Lang::get('confide::confide.username') }}}" type="text" name="username" id="username" value="{{{ Input::old('username') }}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">{{{ Lang::get('confide::confide.e_mail') }}} <small>{{ Lang::get('confide::confide.signup.confirmation_required') }}</small></label>
+                        <input class="form-control" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+                    </div>
+
+
+
+                    <div class="form-group">
+                        <label for="store_id">{{{ Lang::get('confide::confide.store_id') }}} </label>
+                  <select name="store_id" id="store_id" class="form-control">
+                    <?php
+                    $stores = Store::getAllStores();
+                    foreach($stores as $store){
+                      echo "<option value='".$store->store_number."'>".$store->store_number." ".$store->store_name."</option>";
+                    }
+                    ?>
+                  </select>
+                </div>
+
+                    <div class="form-group">
+                        <label for="password">{{{ Lang::get('confide::confide.password') }}}</label>
+                        <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation">{{{ Lang::get('confide::confide.password_confirmation') }}}</label>
+                        <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password_confirmation') }}}" type="password" name="password_confirmation" id="password_confirmation">
+                    </div>
+
+                    @if (Session::get('error'))
+                        <div class="alert alert-error alert-danger">
+                            @if (is_array(Session::get('error')))
+                                {{ head(Session::get('error')) }}
+                            @endif
+                        </div>
+                    @endif
+
+                    @if (Session::get('notice'))
+                        <div class="alert alert-warning">{{ Session::get('notice') }}</div>
+                    @endif
+
+                    <div class="form-actions form-group">
+                      <button type="submit" class="btn btn-primary">{{{ Lang::get('confide::confide.signup.submit') }}}</button>
+                    </div>
+
+                </fieldset>
+            </form>
+
+
 		  			</div>
 				</div>
 	</div>
